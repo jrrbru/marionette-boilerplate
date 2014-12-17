a076daff9b32038b49d391a8ef9e3d4adcc908a8
Jason Brubaker <jbrubaker@wombatsecurity.com>
2014-12-17 13:09:36 -0500

    Updated page template to take advantage of dust's support of partials

--- js/views/PageView.js
+++ js/views/PageView.js
@@ -13,7 +13,7 @@ define([
 
         ui: {
             header: 'h2'
-        },
+        }/*,
 
         onBeforeRender: function(){
 			var This = this;
@@ -22,7 +22,7 @@ define([
 				This.model.set('content', output);
 			});
 
-        }
+        }*/
 
 	});
 });
